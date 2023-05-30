02d9b983798f2f51afbcb1b29ae4e1a2e61ee806
Melanie Mai <melaniesmai@outlook.com>
2023-05-30 13:47:38 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `testingexcel`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
